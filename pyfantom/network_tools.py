b5c2f7e6e095c1d51109f7af4b3f1f8db0e320f7
Gregor Sturm <mail@gregor-sturm.de>
2016-10-05 11:38:17 +0200

    Create unittest in network_tools

--- pyfantom/network_tools.py
+++ pyfantom/network_tools.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from orangecontrib.bio.ontology import OBOOntology
 import numpy as np
 import re
 import itertools
@@ -36,9 +37,30 @@ def relabel_nodes(obo, graph):
     return nx.relabel_nodes(graph, dict([(name, tag2name(obo, name)) for name in graph.nodes()]))
 
 
+def annotate_graph(graph, annotation_df, df_col='obo_id'):
+    """Add annotation to a networkx graph"""
+    for node in graph.nodes():
+        row = annotation_df[annotation_df[df_col] == node]
+        assert 0 <= len(row.index) <= 2     # 0 if the node is not a sample, 2 if there are technical replicates
+        if len(row.index) > 0:
+            for colname, value in zip(row.columns.values, row.iloc[0]):  # iloc 0 in case there are technical replicates
+                if colname == "name":
+                    continue  # dont overwrite native graph attributes
+                graph.node[node][colname] = numpy_conv(value)
+    return graph
+
+
+def numpy_conv(var):
+    """convert numpy datatype to python's standard data types"""
+    if isinstance(var, np.generic):
+        return np.asscalar(var)
+    else:
+        return var
+
+
 def build_tree(obo, term, graph=nx.Graph(), maxdepth=None, filter_fun=lambda x: True):
     """
-    Recursively build a nxnetwork graph starting with 'term'.
+    Recursively build a nxnetwork graph starting with 'term' (top-down).
 
     Builds the subgraph using an OBOOntology.
 
@@ -74,30 +96,11 @@ def build_and_export_tree(obo, term, filename, maxdepth=None, annotation_df=None
     return graph_
 
 
-def annotate_graph(graph, annotation_df, df_col='obo_id'):
-    """Add annotation to a networkx graph"""
-    for node in graph.nodes():
-        row = annotation_df[annotation_df[df_col] == node]
-        assert 0 <= len(row.index) <= 2     # 0 if the node is not a sample, 2 if there are technical replicates
-        if len(row.index) > 0:
-            for colname, value in zip(row.columns.values, row.iloc[0]):  # iloc 0 in case there are technical replicates
-                if colname == "name":
-                    continue  # dont overwrite native graph attributes
-                graph.node[node][colname] = numpy_conv(value)
-    return graph
-
-
-def numpy_conv(var):
-    """convert numpy datatype to python's standard data types"""
-    if isinstance(var, np.generic):
-        return np.asscalar(var)
-    else:
-        return var
-
-
 def add_superelements_to_graph(obo, term, graph, delimiter_nodes, inclusive=False):
     """
-    Add all elements from ontology that are superelements of `obo` and children of `delimiter_nodes`.
+    Add all elements from ontology that are superelements of term and children of `delimiter_nodes`.
+
+    see build_tree_bottom_up_with_delimiters for more detailed explanation.
 
     Args:
         obo: OBOOntology
@@ -120,31 +123,71 @@ def add_superelements_to_graph(obo, term, graph, delimiter_nodes, inclusive=Fals
     for parent_id in parent_ids:
         if intersects(delimiter_nodes, super_term_ids[parent_id]):
             graph.add_edge(term, parent_id)
-            add_superelements_to_graph(obo, parent_id, graph, delimiter_nodes)
+            add_superelements_to_graph(obo, parent_id, graph, delimiter_nodes, inclusive)
     return graph
 
 
-def add_superelements_to_graph_inclusive(obo, term, graph, delimiter_nodes):
-    """
-    Alias of add_superelements_to_graph(..., inclusive=True) [DEPRECATED]
-    """
-    return add_superelements_to_graph(obo, term, graph, delimiter_nodes, True)
-
-
-def add_parents(obo, term, graph, max_depth=None, filter_fun=lambda x: True):
+def build_tree_bottom_up(obo, term, graph, max_depth=None, filter_fun=lambda x: True):
+    """Add all parents of term to the graph with respect to max_depth and filter_fun"""
     parent_ids = [t.id for t in obo.parent_terms(term)]
     if max_depth is None or max_depth > 0:
         for parent_id in parent_ids:
             if filter_fun(parent_id):
                 graph.add_edge(term, parent_id)
-                add_parents(obo, parent_id, graph, max_depth=None if max_depth is None else max_depth-1, filter_fun=filter_fun)
+                build_tree_bottom_up(obo, parent_id, graph, max_depth=None if max_depth is None else max_depth-1, filter_fun=filter_fun)
     return graph
 
 
-def add_superelements_to_graph2(obo, term, graph, delimiter_nodes):
+def build_tree_bottom_up_with_delimiters(obo, term, graph, delimiter_nodes):
+    """
+    Add all elements from ontology that are superelements of term and children
+    of all delimiter nodes on the same branch.
+
+    This is essentially the same as add_superelements_to_graph, except that that
+    function considers children of *any* delimiter node.
+
+    Example:
+         ---------- A ---------
+        |                      |
+        B[delim]               E
+        |                      |
+        C                      F[delim]
+        |
+        D[delim]
+
+    This method will add only A and E, as B and F are delimiter nodes and excluded.
+    add_superelements_to_graph will add A, B, C, E.
+
+    Args:
+        obo: OBOOntology
+        term: obo id, add the superelements of this term
+        graph: networkx graph to add the elements to. Nodes must be named with obo-ids
+        delimiter_nodes: list of nodes that serve as stopping criterion.
+
+    >>> obo = OBOOntology()
+    >>> obo.load(open("test/testdata/dummy_ontology.obo"))
+    >>> graph = nx.Graph()
+    >>> graph = build_tree_bottom_up_with_delimiters(obo, "A", graph, ["B", "D", "F"])
+    >>> sorted(graph.nodes())
+    ['A', 'E']
+    >>> graph = nx.Graph()
+    >>> graph = add_superelements_to_graph(obo, "A", graph, ["B", "D", "F"])
+    >>> sorted(graph.nodes())
+    ['A', 'B', 'C', 'E']
+    >>> graph = nx.Graph()
+    >>> graph = add_superelements_to_graph(obo, "A", graph, ["B", "D", "F"], inclusive=True)
+    >>> sorted(graph.nodes())
+    ['A', 'B', 'C', 'D', 'E', 'F']
+    >>> graph = nx.Graph()
+    >>> graph = build_tree_bottom_up(obo, "A", graph, max_depth=2)
+    >>> sorted(graph.nodes())
+    ['A', 'B', 'C', 'E', 'F']
+    """
     parent_ids = [t.id for t in obo.parent_terms(term)]
     for parent_id in parent_ids:
         # if a parent is in delimiter_nodes we discard that branch.
         if parent_id not in delimiter_nodes:
             graph.add_edge(term, parent_id)
-            add_superelements_to_graph2(obo, parent_id, graph, delimiter_nodes)
+            build_tree_bottom_up_with_delimiters(obo, parent_id, graph, delimiter_nodes)
+    return graph
+
